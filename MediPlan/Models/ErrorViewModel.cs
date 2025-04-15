01cb70b4ad182be541f022e5688773f75c303970
emina54 <ehadzic5@etf.unsa.ba>
2025-04-15 16:24:47 +0200

    PROJEKT

---
+++ MediPlan/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MediPlan.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
