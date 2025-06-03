62a478eedaafd5aa886b43af9ce50878b2fd844a
dtankovic <dtankovic1@etf.unsa.ba>
2025-06-03 15:52:32 +0200

    tipovi usluga izgled

--- MediPlan/Controllers/HomeController.cs
+++ MediPlan/Controllers/HomeController.cs
@@ -1,12 +1,51 @@
 using Microsoft.AspNetCore.Mvc;
+using MediPlan.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MediPlan.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private static readonly List<Usluga> _usluge = new List<Usluga>
         {
-            return View();
+            new Usluga { Naziv = "Interna medicina", Opis = "Opis za internu medicinu...", Ikona = "ikone/internal-medicine.png", Vrsta = VrstaMedicinskihUsluga.InternaMedicina },
+            new Usluga { Naziv = "Kardiologija", Opis = "Opis za kardiologiju...", Ikona = "ikone/cardiology.png", Vrsta = VrstaMedicinskihUsluga.Kardiologija },
+            new Usluga { Naziv = "Oftamologija", Opis = "Opis za oftamologiju...", Ikona = "ikone/ophthalmology.png", Vrsta = VrstaMedicinskihUsluga.Oftamologija },
+            new Usluga { Naziv = "Dermatologija", Opis = "Opis za dermatologiju...", Ikona = "ikone/dermatology.png", Vrsta = VrstaMedicinskihUsluga.Dermatologija },
+            new Usluga { Naziv = "Endokrinologija", Opis = "Opis za endokrinologiju...", Ikona = "ikone/endocrinology.png", Vrsta = VrstaMedicinskihUsluga.Endokrinologija },
+            new Usluga { Naziv = "Ginekologija", Opis = "Opis za ginekologiju...", Ikona = "images/uterus-icon.svg", Vrsta = VrstaMedicinskihUsluga.Ginekologija },
+            new Usluga { Naziv = "Neurologija", Opis = "Opis za neurologiju...", Ikona = "ikone/neurology.png", Vrsta = VrstaMedicinskihUsluga.Neurologija },
+            new Usluga { Naziv = "Radiologija", Opis = "Opis za radiologiju...", Ikona = "ikone/radiology.png", Vrsta = VrstaMedicinskihUsluga.Radiologija }
+        };
+
+        public IActionResult Index() => View();
+
+        public IActionResult About() => View();
+
+        public IActionResult Usluge()
+        {
+            return View(_usluge);
         }
+
+        public IActionResult DetaljiUsluge(string naziv)
+        {
+            if (string.IsNullOrEmpty(naziv))
+                return RedirectToAction("Usluge");
+
+            var usluga = _usluge.FirstOrDefault(u => u.Naziv == naziv);
+            if (usluga == null)
+                return NotFound();
+
+            return View(usluga);
+        }
+
+        public IActionResult Tim() => View();
+
+        public IActionResult Kontakt() => View();
+
+        public IActionResult Zakazi() => View();
+
+        public IActionResult Privacy() => View();
     }
 }
--- MediPlan/obj/Debug/net8.0/MediPlan.AssemblyInfo.cs
+++ MediPlan/obj/Debug/net8.0/MediPlan.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MediPlan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cca3f71dcae264364bccd68128595546688b0d4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+254f8606c982085f4b192e1449d4a8a5d0200ed9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MediPlan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MediPlan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
