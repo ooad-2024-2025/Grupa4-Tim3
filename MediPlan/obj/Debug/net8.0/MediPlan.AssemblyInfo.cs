958b300d6492eaf70657acfff7daf368392b5b4b
emina54 <ehadzic5@etf.unsa.ba>
2025-06-03 13:29:38 +0200

    termini

--- MediPlan/obj/Debug/net8.0/MediPlan.AssemblyInfo.cs
+++ MediPlan/obj/Debug/net8.0/MediPlan.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MediPlan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c033e3a059b1f58823d754a81e29541e0a1cbc7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+646a49143b446da97b3e72da5d380df83e5c8e1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MediPlan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MediPlan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
