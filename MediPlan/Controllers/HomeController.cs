ce9131c5fdce9c5892cad4103cbb856ddc5f8d14
LamijaRa <lramic2@etf.unsa.ba>
2025-06-05 00:11:04 +0200

    Update HomeController.cs

--- MediPlan/Controllers/HomeController.cs
+++ MediPlan/Controllers/HomeController.cs
@@ -30,13 +30,62 @@ namespace MediPlan.Controllers
 
         // Testni podaci za korisnike (doktore)
         private static readonly List<Korisnik> _korisnici = new List<Korisnik>
-        {
-            new Korisnik { Id = 1, Ime = "Marko", Prezime = "Markovic", Email = "marko@primjer.com", Uloga = Uloga.Doktor, Odjel = Odjel.InternaMedicina },
-            new Korisnik { Id = 2, Ime = "Ana", Prezime = "Anic", Email = "ana@primjer.com", Uloga = Uloga.Doktor, Odjel = Odjel.Kardiologija },
-            new Korisnik { Id = 3, Ime = "Ivan", Prezime = "Ivanovic", Email = "ivan@primjer.com", Uloga = Uloga.Doktor, Odjel = Odjel.Kardiologija },
-            new Korisnik { Id = 4, Ime = "Jelena", Prezime = "Jelic", Email = "jelena@primjer.com", Uloga = Uloga.Doktor, Odjel = Odjel.Oftamologija },
-            // Dodaj još doktora po potrebi
-        };
+{
+    // Interna medicina
+    new Korisnik { Id = 1, Ime = "Amar", Prezime = "Husic", Email = "amar.husic@mediplan.ba", Uloga = Uloga.Doktor, Odjel = Odjel.InternaMedicina },
+    new Korisnik { Id = 2, Ime = "Lejla", Prezime = "Fazlic", Email = "lejla.fazlic@mediplan.ba", Uloga = Uloga.Doktor, Odjel = Odjel.InternaMedicina },
+    new Korisnik { Id = 3, Ime = "Dino", Prezime = "Selimovic", Email = "dino.selimovic@mediplan.ba", Uloga = Uloga.Doktor, Odjel = Odjel.InternaMedicina },
+    new Korisnik { Id = 4, Ime = "Sara", Prezime = "Softic", Email = "sara.softic@mediplan.ba", Uloga = Uloga.Doktor, Odjel = Odjel.InternaMedicina },
+
+    // Kardiologija
+    new Korisnik { Id = 5, Ime = "Ana", Prezime = "Anic", Email = "ana.anic@mediplan.ba", Uloga = Uloga.Doktor, Odjel = Odjel.Kardiologija },
+    new Korisnik { Id = 6, Ime = "Ivan", Prezime = "Ivanovic", Email = "ivan.ivanovic@mediplan.ba", Uloga = Uloga.Doktor, Odjel = Odjel.Kardiologija },
+    new Korisnik { Id = 7, Ime = "Mina", Prezime = "Milosavljevic", Email = "mina.milosavljevic@mediplan.ba", Uloga = Uloga.Doktor, Odjel = Odjel.Kardiologija },
+    new Korisnik { Id = 8, Ime = "Faruk", Prezime = "Dzafic", Email = "faruk.dzafic@mediplan.ba", Uloga = Uloga.Doktor, Odjel = Odjel.Kardiologija },
+
+    // Oftamologija
+    new Korisnik { Id = 9, Ime = "Jelena", Prezime = "Jelic", Email = "jelena.jelic@mediplan.ba", Uloga = Uloga.Doktor, Odjel = Odjel.Oftamologija },
+    new Korisnik { Id = 10, Ime = "Tarik", Prezime = "Zulic", Email = "tarik.zulic@mediplan.ba", Uloga = Uloga.Doktor, Odjel = Odjel.Oftamologija },
+    new Korisnik { Id = 11, Ime = "Azra", Prezime = "Balic", Email = "azra.balic@mediplan.ba", Uloga = Uloga.Doktor, Odjel = Odjel.Oftamologija },
+    new Korisnik { Id = 12, Ime = "Edin", Prezime = "Kovacevic", Email = "edin.kovacevic@mediplan.ba", Uloga = Uloga.Doktor, Odjel = Odjel.Oftamologija },
+
+    // Dermatologija
+    new Korisnik { Id = 13, Ime = "Emir", Prezime = "Emirovic", Email = "emir.emirovic@mediplan.ba", Uloga = Uloga.Doktor, Odjel = Odjel.Dermatologija },
+    new Korisnik { Id = 14, Ime = "Lejla", Prezime = "Lekic", Email = "lejla.lekic@mediplan.ba", Uloga = Uloga.Doktor, Odjel = Odjel.Dermatologija },
+    new Korisnik { Id = 15, Ime = "Selma", Prezime = "Hasanovic", Email = "selma.hasanovic@mediplan.ba", Uloga = Uloga.Doktor, Odjel = Odjel.Dermatologija },
+    new Korisnik { Id = 16, Ime = "Aldin", Prezime = "Alikadic", Email = "aldin.alikadic@mediplan.ba", Uloga = Uloga.Doktor, Odjel = Odjel.Dermatologija },
+
+    // Endokrinologija
+    new Korisnik { Id = 17, Ime = "Sara", Prezime = "Saric", Email = "sara.saric@mediplan.ba", Uloga = Uloga.Doktor, Odjel = Odjel.Endokrinologija },
+    new Korisnik { Id = 18, Ime = "Adnan", Prezime = "Ademovic", Email = "adnan.ademovic@mediplan.ba", Uloga = Uloga.Doktor, Odjel = Odjel.Endokrinologija },
+    new Korisnik { Id = 19, Ime = "Irma", Prezime = "Herceg", Email = "irma.herceg@mediplan.ba", Uloga = Uloga.Doktor, Odjel = Odjel.Endokrinologija },
+    new Korisnik { Id = 20, Ime = "Nedim", Prezime = "Ibric", Email = "nedim.ibric@mediplan.ba", Uloga = Uloga.Doktor, Odjel = Odjel.Endokrinologija },
+
+    // Ginekologija
+    new Korisnik { Id = 21, Ime = "Maja", Prezime = "Majic", Email = "maja.majic@mediplan.ba", Uloga = Uloga.Doktor, Odjel = Odjel.Ginekologija },
+    new Korisnik { Id = 22, Ime = "Elma", Prezime = "Hadzic", Email = "elma.hadzic@mediplan.ba", Uloga = Uloga.Doktor, Odjel = Odjel.Ginekologija },
+    new Korisnik { Id = 23, Ime = "Aida", Prezime = "Brkic", Email = "aida.brkic@mediplan.ba", Uloga = Uloga.Doktor, Odjel = Odjel.Ginekologija },
+    new Korisnik { Id = 24, Ime = "Senad", Prezime = "Begovic", Email = "senad.begovic@mediplan.ba", Uloga = Uloga.Doktor, Odjel = Odjel.Ginekologija },
+
+    // Neurologija
+    new Korisnik { Id = 25, Ime = "Elma", Prezime = "Elmaz", Email = "elma.elmaz@mediplan.ba", Uloga = Uloga.Doktor, Odjel = Odjel.Neurologija },
+    new Korisnik { Id = 26, Ime = "Vedad", Prezime = "Velic", Email = "vedad.velic@mediplan.ba", Uloga = Uloga.Doktor, Odjel = Odjel.Neurologija },
+    new Korisnik { Id = 27, Ime = "Selma", Prezime = "Mujic", Email = "selma.mujic@mediplan.ba", Uloga = Uloga.Doktor, Odjel = Odjel.Neurologija },
+    new Korisnik { Id = 28, Ime = "Dzenan", Prezime = "Begic", Email = "dzenan.begic@mediplan.ba", Uloga = Uloga.Doktor, Odjel = Odjel.Neurologija },
+
+    // Radiologija
+    new Korisnik { Id = 29, Ime = "Tarik", Prezime = "Tarikovic", Email = "tarik.tarikovic@mediplan.ba", Uloga = Uloga.Doktor, Odjel = Odjel.Radiologija },
+    new Korisnik { Id = 30, Ime = "Anesa", Prezime = "Dedic", Email = "anesa.dedic@mediplan.ba", Uloga = Uloga.Doktor, Odjel = Odjel.Radiologija },
+    new Korisnik { Id = 31, Ime = "Edina", Prezime = "Zukic", Email = "edina.zukic@mediplan.ba", Uloga = Uloga.Doktor, Odjel = Odjel.Radiologija },
+    new Korisnik { Id = 32, Ime = "Benjamin", Prezime = "Osmanovic", Email = "benjamin.osmanovic@mediplan.ba", Uloga = Uloga.Doktor, Odjel = Odjel.Radiologija },
+
+    // Laboratorijske usluge
+    new Korisnik { Id = 33, Ime = "Ines", Prezime = "Mahmutovic", Email = "ines.mahmutovic@mediplan.ba", Uloga = Uloga.Doktor, Odjel = Odjel.LaboratorijskeUsluge },
+    new Korisnik { Id = 34, Ime = "Haris", Prezime = "Memic", Email = "haris.memic@mediplan.ba", Uloga = Uloga.Doktor, Odjel = Odjel.LaboratorijskeUsluge },
+    new Korisnik { Id = 35, Ime = "Amila", Prezime = "Sinanovic", Email = "amila.sinanovic@mediplan.ba", Uloga = Uloga.Doktor, Odjel = Odjel.LaboratorijskeUsluge },
+    new Korisnik { Id = 36, Ime = "Damir", Prezime = "Basic", Email = "damir.basic@mediplan.ba", Uloga = Uloga.Doktor, Odjel = Odjel.LaboratorijskeUsluge },
+};
+
 
         public IActionResult Index() => View();
 
@@ -70,11 +119,31 @@ namespace MediPlan.Controllers
             return View(model);
         }
 
-        public IActionResult Tim() => View();
+        
 
         public IActionResult Kontakt() => View();
+       
+        [HttpPost]
+        public IActionResult Kontakt(string ime, string email, string poruka)
+        {
+            TempData["Poruka"] = "Hvala na poruci, " + ime + "! Odgovorit ćemo vam uskoro.";
+            return RedirectToAction("Kontakt");
+        }
+
 
         public IActionResult Zakazi() => View();
+        public IActionResult NasTim()
+        {
+            var model = _korisnici
+                .Where(k => k.Uloga == Uloga.Doktor)
+                .GroupBy(k => k.Odjel)
+                .ToDictionary(g => g.Key, g => g.ToList());
+
+            return View(model);
+        }
+
+
+
 
         public IActionResult Privacy() => View();
     }
