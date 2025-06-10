a866ce8f671ace730f8958de78580a0f897936e5
amilababic4 <ababic4@etf.unsa.ba>
2025-06-10 20:39:30 +0200

    Login register rade
    L

---
+++ MediPlan/Controllers/AccountContoller.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace MediPlan.Controllers
+{
+
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        // Na primer, profil stranica korisnika
+        [HttpGet]
+        public async Task<IActionResult> Profile()
+        {
+            if (!_signInManager.IsSignedIn(User))
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            // Ako želiš da dobaviš dodatne podatke (npr. claimove, uloge, itd.)
+            var roles = await _userManager.GetRolesAsync(user);
+            ViewBag.Roles = roles;
+
+            return View(user);
+        }
+
+        // Logout logika
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
--- MediPlan/obj/Release/net8.0/MediPlan.AssemblyInfo.cs
+++ MediPlan/obj/Release/net8.0/MediPlan.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MediPlan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91e6f31b9afdb9d36c49ff9d14afbf777f52dba3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edb23ef828a75d45e591ed37b4b232ab6c27c328")]
 [assembly: System.Reflection.AssemblyProductAttribute("MediPlan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MediPlan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
