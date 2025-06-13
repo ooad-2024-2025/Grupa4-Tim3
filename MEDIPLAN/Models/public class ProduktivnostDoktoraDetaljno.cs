ea6deaa76da0dd35c62f26ecba7818ae73d7083d
emina54 <ehadzic5@etf.unsa.ba>
2025-06-13 17:03:11 +0200

    pregledi statistika

---
+++ MEDIPLAN/Models/public class ProduktivnostDoktoraDetaljno.cs
@@ -0,0 +1,11 @@
+﻿namespace MEDIPLAN.Models
+{
+    public class ProduktivnostDoktoraDetaljno
+    {
+        public int DoktorId { get; set; }
+        public string ImeDoktora { get; set; }
+        public int Godina { get; set; }
+        public int Mjesec { get; set; }
+        public int BrojPregleda { get; set; }
+    }
+}
