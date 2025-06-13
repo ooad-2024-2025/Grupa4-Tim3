7e74a74ac13c269e8a26800f97fb70fc8cd72c77
emina54 <ehadzic5@etf.unsa.ba>
2025-06-13 16:04:36 +0200

    pacijenti po godinama i mjesecu

--- MEDIPLAN/Controllers/HomeController.cs
+++ MEDIPLAN/Controllers/HomeController.cs
@@ -2,115 +2,143 @@
 using MEDIPLAN.Models;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using MEDIPLAN.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace MEDIPLAN.Controllers
 {
-	public class HomeController : Controller
-	{
-		private readonly ApplicationDbContext _context;
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
 
-		// Konstruktor za Dependency Injection DbContext-a
-		public HomeController(ApplicationDbContext context)
-		{
-			_context = context;
-		}
+        // Konstruktor za Dependency Injection DbContext-a
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
 
-		public IActionResult Index() => View();
+        public IActionResult Index() => View();
 
-		public IActionResult About() => View();
+        public IActionResult About() => View();
 
-		public IActionResult Usluge()
-		{
-			// Hardkodirana lista usluga
-			var usluge = new List<Usluge>
-			{
-				new Usluge { Naziv = "Interna Medicina", Ikona = "images/interna-medicina.png" },
-				new Usluge { Naziv = "Kardiologija", Ikona = "images/kardiologija.png" },
-				new Usluge { Naziv = "Oftamologija", Ikona = "images/oftamologija.png" },
-				new Usluge { Naziv = "Dermatologija", Ikona = "images/dermatologija.png" },
-				new Usluge { Naziv = "Endokrinologija", Ikona = "images/endokrinologija.png" },
-				new Usluge { Naziv = "Ginekologija", Ikona = "images/gynecology.png" },
-				new Usluge { Naziv = "Neurologija", Ikona = "images/neurologija.png" },
-				new Usluge { Naziv = "Radiologija", Ikona = "images/radiology.png" }
-			};
-
-			return View(usluge);
-		}
-
-		public IActionResult DetaljiUsluge(string naziv)
-		{
-			if (string.IsNullOrEmpty(naziv))
-				return RedirectToAction("Usluge");
-
-			var usluga = _context.Usluge.FirstOrDefault(u => u.Naziv == naziv);
-			if (usluga == null)
-				return NotFound();
-
-			var doktoriZaUslugu = _context.Korisnici
-				.Where(k => k.Uloga == (int)Uloga.Doktor && k.Odjel == usluga.Odjel)
-				.ToList();
-
-			var model = new DetaljiUslugeViewModel
-			{
-				Usluge = usluga,
-				Doktori = doktoriZaUslugu
-			};
-
-			return View(model);
-		}
-
-
-
-       
-
-            public IActionResult Statistika()
+        public IActionResult Usluge()
+        {
+            // Hardkodirana lista usluga
+            var usluge = new List<Usluge>
             {
-                // Ovdje možeš pripremiti model sa podacima za statistiku ako treba
-                return View();
-            }
-        
+                new Usluge { Naziv = "Interna Medicina", Ikona = "images/interna-medicina.png" },
+                new Usluge { Naziv = "Kardiologija", Ikona = "images/kardiologija.png" },
+                new Usluge { Naziv = "Oftamologija", Ikona = "images/oftamologija.png" },
+                new Usluge { Naziv = "Dermatologija", Ikona = "images/dermatologija.png" },
+                new Usluge { Naziv = "Endokrinologija", Ikona = "images/endokrinologija.png" },
+                new Usluge { Naziv = "Ginekologija", Ikona = "images/gynecology.png" },
+                new Usluge { Naziv = "Neurologija", Ikona = "images/neurologija.png" },
+                new Usluge { Naziv = "Radiologija", Ikona = "images/radiology.png" }
+            };
+
+            return View(usluge);
+        }
+
+        public IActionResult DetaljiUsluge(string naziv)
+        {
+            if (string.IsNullOrEmpty(naziv))
+                return RedirectToAction("Usluge");
+
+            var usluga = _context.Usluge.FirstOrDefault(u => u.Naziv == naziv);
+            if (usluga == null)
+                return NotFound();
+
+            var doktoriZaUslugu = _context.Korisnici
+                .Where(k => k.Uloga == (int)Uloga.Doktor && k.Odjel == usluga.Odjel)
+                .ToList();
+
+            var model = new DetaljiUslugeViewModel
+            {
+                Usluge = usluga,
+                Doktori = doktoriZaUslugu
+            };
+
+            return View(model);
+        }
+
+        // ** DODATAK: Statistika akcija **
+        public async Task<IActionResult> Statistika()
+        {
+            // Grupisanje po godini i mjesecu, broj jedinstvenih pacijenata po mjesecu
+            var preglediPoMjesecima = await _context.Termini
+                .GroupBy(t => new { Godina = t.DatumVrijemePocetak.Year, Mjesec = t.DatumVrijemePocetak.Month })
+                .Select(g => new
+                {
+                    Godina = g.Key.Godina,
+                    Mjesec = g.Key.Mjesec,
+                    BrojPacijenata = g.Select(x => x.PacijentId).Distinct().Count()
+                })
+                .OrderByDescending(x => x.Godina).ThenByDescending(x => x.Mjesec)
+                .ToListAsync();
+
+            // Ukupno jedinstvenih pacijenata kroz cijelo vrijeme
+            var ukupnoPacijenata = await _context.Termini
+                .Select(t => t.PacijentId)
+                .Distinct()
+                .CountAsync();
+
+            // Mapiranje u ViewModel
+            var model = new StatistikaViewModel
+            {
+                PreglediPoMjesecima = preglediPoMjesecima.Select(x => new PregledPoMjesecu
+                {
+                    Godina = x.Godina,
+                    Mjesec = x.Mjesec,
+                    BrojPregleda = x.BrojPacijenata
+                }).ToList(),
+
+                UkupnoPacijenata = ukupnoPacijenata
+            };
+
+            return View(model);
+        }
 
         public IActionResult Kontakt()
-		{
-			var lokacije = new List<LokacijaInfo>
-			{
-				new LokacijaInfo
-				{
-					Grad = "Sarajevo",
-					Adresa = "Zmaja od Bosne 12",
-					Telefon = "+387 33 123 456",
-					Email = "info@MEDIPLAN.ba",
-					Slika = "images/grbavica.jpg"
-				},
-				new LokacijaInfo
-				{
-					Grad = "Ilidža",
-					Adresa = "Butmirska Cesta 9",
-					Telefon = "+387 36 654 321",
-					Email = "info@MEDIPLAN.ba",
-					Slika = "images/butmir.jpg"
-				}
-			};
+        {
+            var lokacije = new List<LokacijaInfo>
+            {
+                new LokacijaInfo
+                {
+                    Grad = "Sarajevo",
+                    Adresa = "Zmaja od Bosne 12",
+                    Telefon = "+387 33 123 456",
+                    Email = "info@MEDIPLAN.ba",
+                    Slika = "images/grbavica.jpg"
+                },
+                new LokacijaInfo
+                {
+                    Grad = "Ilidža",
+                    Adresa = "Butmirska Cesta 9",
+                    Telefon = "+387 36 654 321",
+                    Email = "info@MEDIPLAN.ba",
+                    Slika = "images/butmir.jpg"
+                }
+            };
 
-			return View(lokacije);
-		}
+            return View(lokacije);
+        }
 
-		public IActionResult MojProfil()
-		{
-			return View();
-		}
+        public IActionResult MojProfil()
+        {
+            return View();
+        }
 
-		public IActionResult Cjenovnik()
-		{
-			return View();
-		}
+        public IActionResult Cjenovnik()
+        {
+            return View();
+        }
 
-		public IActionResult ZakaziTermini()
-		{
-			return View();
-		}
+        public IActionResult ZakaziTermini()
+        {
+            return View();
+        }
 
-		public IActionResult Privacy() => View();
-	}
+        public IActionResult Privacy() => View();
+    }
 }
--- MEDIPLAN/obj/Release/net8.0/MEDIPLAN.AssemblyInfo.cs
+++ MEDIPLAN/obj/Release/net8.0/MEDIPLAN.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MEDIPLAN")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab32a7dcfc9cab7a6569d44a5e39ae6b8dfe63c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7180b9292021326ccaa2e54d2dcda3e356ab255")]
 [assembly: System.Reflection.AssemblyProductAttribute("MEDIPLAN")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MEDIPLAN")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
