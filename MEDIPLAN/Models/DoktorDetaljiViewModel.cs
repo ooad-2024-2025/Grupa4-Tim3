1ededcb2ad453db9c8520b240cc0f5d1b0f181fb
LamijaRa <lramic2@etf.unsa.ba>
2025-06-14 21:56:50 +0200

    Create DoktorDetaljiViewModel.cs

---
+++ MEDIPLAN/Models/DoktorDetaljiViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using MEDIPLAN.Models;
+
+namespace MEDIPLAN.ViewModels
+{
+    public class DoktorDetaljiViewModel
+    {
+        public Korisnici Doktor { get; set; } = null!;
+        public List<Recenzija> Recenzije { get; set; } = new List<Recenzija>();
+
+        public double ProsjekOcjenaDoktor { get; set; }
+        public double ProsjekOcjenaKlinika { get; set; }
+    }
+}
