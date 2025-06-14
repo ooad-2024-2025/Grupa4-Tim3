03ec78cc7a1759e50d1a24b316a820983b81e4d5
dtankovic <dtankovic1@etf.unsa.ba>
2025-06-14 15:14:16 +0200

    view doktor

--- MEDIPLAN/obj/Release/net8.0/MEDIPLAN.AssemblyInfo.cs
+++ MEDIPLAN/obj/Release/net8.0/MEDIPLAN.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MEDIPLAN")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b20183288b07763172fe2d417bde2e3760abdf2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0babd8693f2407661bd04c417e3c7084a549777f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MEDIPLAN")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MEDIPLAN")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
