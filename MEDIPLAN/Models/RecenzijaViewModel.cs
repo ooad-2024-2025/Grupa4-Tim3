397c8f928a9726969b54aa0d4a0d1b3a8fca3f5c
LamijaRa <lramic2@etf.unsa.ba>
2025-06-14 21:58:17 +0200

    Create RecenzijaViewModel.cs

---
+++ MEDIPLAN/Models/RecenzijaViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MEDIPLAN.Models;
+
+namespace MEDIPLAN.Models
+{
+    public class RecenzijaViewModel
+    {
+        public int TerminId { get; set; }
+        public int DoktorId { get; set; }
+
+        [Range(1, 5)]
+        public int OcjenaDoktor { get; set; }
+
+        [Range(1, 5)]
+        public int OcjenaKlinika { get; set; }
+
+        [Required]
+        [StringLength(1000)]
+        public string Tekst { get; set; } = string.Empty;
+
+        public string DoktorIme { get; set; } = string.Empty;
+    }
+}
