599f35c01a189c3f82171d11ea8351ae3e4f9331
LamijaRa <lramic2@etf.unsa.ba>
2025-06-14 21:58:29 +0200

    Update Recenzije.cs

--- MEDIPLAN/Models/Recenzije.cs
+++ MEDIPLAN/Models/Recenzije.cs
@@ -1,15 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using MEDIPLAN.Models;
 
-namespace MEDIPLAN.Models;
 
-public partial class Recenzije
+namespace MEDIPLAN.Models
 {
-    public int Id { get; set; }
+    public class Recenzija
+    {
+        public int Id { get; set; }
 
-    public string ImeKorisnika { get; set; } = null!;
+       
+        public string Tekst { get; set; } = string.Empty;
 
-    public string Tekst { get; set; } = null!;
+        public DateTime Datum { get; set; }
 
-    public DateTime Datum { get; set; }
+        public int KorisnikId { get; set; }
+        public Korisnici Korisnik { get; set; } = null!;  
+        public int DoktorId { get; set; }
+        public Korisnici Doktor { get; set; } = null!;
+
+        public int TerminId { get; set; }
+        public Termini Termin { get; set; } = null!;
+
+        public int OcjenaDoktor { get; set; }
+        public int OcjenaKlinika { get; set; }
+    }
 }
