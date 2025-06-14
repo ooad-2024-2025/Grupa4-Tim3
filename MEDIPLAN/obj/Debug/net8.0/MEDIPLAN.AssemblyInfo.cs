fc09110d5aba33084c9b563914c337ab58f8e6ef
emina54 <ehadzic5@etf.unsa.ba>
2025-06-14 12:41:16 +0200

    izmjene

--- MEDIPLAN/obj/Debug/net8.0/MEDIPLAN.AssemblyInfo.cs
+++ MEDIPLAN/obj/Debug/net8.0/MEDIPLAN.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MEDIPLAN")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fdd2046e585aedde3b3f1aaf98d1062842c0627")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14cf0d9f6485835adbcc6760c2c50053f405c6ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("MEDIPLAN")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MEDIPLAN")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MEDIPLAN/obj/Release/net8.0/MEDIPLAN.AssemblyInfo.cs
+++ MEDIPLAN/obj/Release/net8.0/MEDIPLAN.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MEDIPLAN")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dde278fb83e262c2bf5ae4962d59dc1aaf3cd45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14cf0d9f6485835adbcc6760c2c50053f405c6ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("MEDIPLAN")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MEDIPLAN")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
