cd40848bd9335a0f960c21f9f41eaa3d46a97798
LamijaRa <lramic2@etf.unsa.ba>
2025-06-15 08:46:37 +0200

    Recenzije

--- MEDIPLAN/Controllers/RecenzijeController.cs
+++ MEDIPLAN/Controllers/RecenzijeController.cs
@@ -1,1 +1,31 @@
-﻿
+﻿using MEDIPLAN.Data;
+using MEDIPLAN.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace MEDIPLAN.Controllers
+{
+    public class RecenzijeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RecenzijeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var recenzije = _context.Recenzije
+                .Where(r => r.OcjenaKlinika > 0)
+                .Include(r => r.Korisnik)
+                .ToList();
+
+            var prosjekKlinike = recenzije.Any() ? recenzije.Average(r => r.OcjenaKlinika) : 0;
+
+            ViewBag.ProsjekKlinike = prosjekKlinike;
+            return View(recenzije);
+        }
+    }
+}
--- MEDIPLAN/obj/Debug/net8.0/MEDIPLAN.AssemblyInfo.cs
+++ MEDIPLAN/obj/Debug/net8.0/MEDIPLAN.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MEDIPLAN")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d90dcd3d14932087fbece80751ad1d738f9d36f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2aa42f64ed3e6d929dcb7ff6c9591d1cc8b4623")]
 [assembly: System.Reflection.AssemblyProductAttribute("MEDIPLAN")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MEDIPLAN")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
