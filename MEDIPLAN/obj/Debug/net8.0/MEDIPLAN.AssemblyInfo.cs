9fbddd02e788746fc7d46ff77bf1b4fd905d852c
LamijaRa <lramic2@etf.unsa.ba>
2025-06-16 16:00:58 +0200

    Izgled i funkcionalnosti recenzija

--- MEDIPLAN/obj/Debug/net8.0/MEDIPLAN.AssemblyInfo.cs
+++ MEDIPLAN/obj/Debug/net8.0/MEDIPLAN.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MEDIPLAN")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ba7bc42ed183b5ea5abd89db5cad1ae98e9f403")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a6aa27cb604e283b59eecc9dffd99d6d19304c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MEDIPLAN")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MEDIPLAN")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
